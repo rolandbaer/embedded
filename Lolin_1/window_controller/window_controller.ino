b9f02af1cdde23113c6942e62314504972225d3b
rolandbaer <rbaer@gmx.ch>
2020-04-05 21:26:20 +0200

    redesing of web UI, added 4 buttons and handling for open and close button

--- Lolin_1/window_controller/window_controller.ino
+++ Lolin_1/window_controller/window_controller.ino
@@ -58,6 +58,11 @@ unsigned long timer, interval, last_measure;
 int button_state_open = 0;
 int button_state_close = 0;
 
+bool event_open = false;
+bool event_close = false;
+bool event_manual = false;
+bool event_auto = false;
+
 // variables for measured values
 float temperature = NAN;
 float humidity = NAN;
@@ -109,7 +114,12 @@ void initWebServer() {
   WiFi.macAddress(macAddr);
   Serial.printf("mac address: %02x:%02x:%02x:%02x:%02x:%02x\n", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
 
-  webServer.on("/", handle_OnConnect);
+  webServer.on("/", handle_IndexHtml);
+  webServer.on("/data", handle_Data);
+  webServer.on("/open", handle_Open);
+  webServer.on("/close", handle_Close);
+  webServer.on("/manual", handle_Manual);
+  webServer.on("/auto", handle_Auto);
   webServer.onNotFound(handle_NotFound);
 
   webServer.begin();
@@ -128,8 +138,8 @@ void loop() {
     Serial.println("");
   }
   
-  state_machine_run((current_state_open != button_state_open) && current_state_open,
-                    (current_state_close != button_state_close) && current_state_close);
+  state_machine_run(process_event_open() || ((current_state_open != button_state_open) && current_state_open),
+                    process_event_close() || ((current_state_close != button_state_close) && current_state_close));
   button_state_open = current_state_open;
   button_state_close = current_state_close;
 
@@ -244,10 +254,55 @@ bool dry_or_cold(float _temperature, float _humidity) {
   return _humidity <= HUMIDITY || _temperature <= TEMPERATURE ;
 }
 
-void handle_OnConnect() {
-  temperature = dht.readTemperature();
-  humidity = dht.readHumidity();
-  webServer.send(200, "text/html", buildHtml(temperature,humidity)); 
+void handle_IndexHtml() {
+  webServer.send(200, "text/html",  buildHtml(temperature,humidity)); 
+}
+
+void handle_Data() {
+  String data = "" + String((int)temperature) + "/" + String((int)humidity) + "/" + printState(state);
+  webServer.send(200, "text/html", data); 
+}
+
+bool process_event_open() {
+  if(event_open) {
+    event_open = false;
+    return true;
+  }
+
+  return false;
+}
+  
+bool process_event_close() {
+  if(event_close) {
+    event_close = false;
+    return true;
+  }
+
+  return false;
+}
+  
+void handle_Open() {
+  Serial.println("*** Open ***");
+  event_open = true;
+  webServer.send(200, "text/html", ""); 
+}
+
+void handle_Close() {
+  Serial.println("*** Close ***");
+  event_close = true;
+  webServer.send(200, "text/html", ""); 
+}
+
+void handle_Auto() {
+  Serial.println("*** Auto ***");
+  event_auto = true;
+  webServer.send(200, "text/html", ""); 
+}
+
+void handle_Manual() {
+  Serial.println("*** Manual ***");
+  event_manual = true;
+  webServer.send(200, "text/html", ""); 
 }
 
 void handle_NotFound(){
@@ -258,31 +313,70 @@ String buildHtml(float _temperature,float _humidity){
   String page = "<!DOCTYPE html> <html>\n";
   page +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
   page +="<meta charset=\"UTF-8\">";
-  page +="<meta http-equiv=\"refresh\" content=\"5\" >";
   page +="<title>WeMos D1 mini Temperature & Humidity Report</title>\n";
-  page +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  page +="body{margin-top: 50px;} h1 {color: #444444;margin: 20px auto 30px;}\n";
-  page +="h2 {color: #0d4c75;margin: 50px auto 20px;}\n";
-  page +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  page +="<link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\" integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">";
+  page +="<style>html {font-family: Helvetica; display: inline-block; margin: 0px auto; width: 32em;}\n";
+  page +="body {margin-top: 50px;} h1 {color: #444444; margin: 20px auto 30px;}\n";
+  page +="h2 {color: #0d4c75; margin: 50px auto 20px; font-size: 3.0rem;}\n";
+  page +="p {font-size: 3.0rem; color: #444444; margin-bottom: 10px;} i {width: 1.1em;}\n";
+  page +="button {font-size: 2.0rem; color: #444444; width: 7em; padding: 8px;}\n";
+  page +=".dht-labels {font-size: 1.5rem; vertical-align:middle; padding-bottom: 15px; width: 8em; display:inline-block;}\n";
+  page +=".units { font-size: 1.5rem; }\n";
   page +="</style>\n";
   page +="</head>\n";
   page +="<body>\n";
   page +="<div id=\"webpage\">\n";
   page +="<h2>WeMos Lolin D1 mini</h2><h1>Temperature & Humidity Report</h1>\n";
   
-  page +="<p>Temperature: ";
-  page +=(int)_temperature;
-  page +=" °C</p>";
-  page +="<p>Humidity: ";
-  page +=(int)_humidity;
-  page +=" %</p>";
-  
-  page +="<p>Window State: ";
+  page +="<p><i class=\"fas fa-thermometer-half\" style=\"color:#059e8a;\"></i>\n"; 
+  page +="<span class=\"dht-labels\">Temperature</span>\n"; 
+  page +="<span id=\"temperature\">";
+  page +=isnan(_temperature)? 0 : (int)_temperature;
+  page +="</span>\n<sup class=\"units\">&deg;C</sup></p>\n";
+
+  page +="<p><i class=\"fas fa-tint\" style=\"color:#00add6;\"></i>\n"; 
+  page +="<span class=\"dht-labels\">Humidity</span>\n";
+  page +="<span id=\"humidity\">";
+  page +=isnan(_humidity)? 0 : (int)_humidity;
+  page +="</span>\n<sup class=\"units\">%</sup></p>\n";
+
+  page +="<p><i class=\"fas fa-eye\" style=\"color:#add600;\"></i>\n"; 
+  page +="<span class=\"dht-labels\">Window State</span>\n";
+  page +="<span id=\"state\">";
   page +=printState(state);
-  page +="</p>"; 
-   
+  page +="</span></p>\n";
+  
+  page +="<p><button onClick=\"sendCommand('open');\"><i class=\"fas fa-share\" style=\"color:#123456;\"></i>\n"; 
+  page +="Open</button>\n";
+  page +="<button onClick=\"sendCommand('close');\"><i class=\"fas fa-reply\" style=\"color:#123456;\"></i>\n"; 
+  page +="Close</button></p>\n";
+  page +="<p><button onClick=\"sendCommand('manual');\"><i class=\"far fa-hand-paper\" style=\"color:#123456;\"></i>\n"; 
+  page +="Manual</button>\n";
+  page +="<button onClick=\"sendCommand('auto');\"><i class=\"fas fa-robot\" style=\"color:#123456;\"></i>\n"; 
+  page +="Auto</button></p>\n";
+  
   page +="</div>\n";
   page +="</body>\n";
+  page +="<script>\n";
+  page +="setInterval(function ( ) {";
+  page +="var xhttp = new XMLHttpRequest();";
+  page +="xhttp.onreadystatechange = function() {";
+  page +="  if (this.readyState == 4 && this.status == 200) {";
+  page +="    var res = this.responseText.split(\"/\");";
+  page +="    document.getElementById(\"temperature\").innerHTML = res[0];";
+  page +="    document.getElementById(\"humidity\").innerHTML = res[1];";
+  page +="    document.getElementById(\"state\").innerHTML = res[2];";
+  page +="  }";
+  page +="};";
+  page +="xhttp.open(\"GET\", \"/data\", true);";
+  page +="xhttp.send();";
+  page +="}, 2000 ) ;";
+  page +="function sendCommand(command) {";
+  page +="  var xhttp = new XMLHttpRequest();";
+  page +="  xhttp.open(\"GET\", \"/\" + command, true);";
+  page +="  xhttp.send();";
+  page +="}";
+  page +="</script>";
   page +="</html>\n";
   return page;
 }
